52e23e3144ed1a0f9f6cf12c306e0fa0146d1654
Karol Gajewski <karoolg91@gmail.com>
2020-03-31 22:26:05 +0200

    adding input to stocks

--- stock-app/src/components/Stocks.js
+++ stock-app/src/components/Stocks.js
@@ -1,15 +1,20 @@
 import React, {useState, useEffect} from 'react';
 import Plot from 'react-plotly.js';
+import stockSymbols from "../stocksSymbols/StockSymbols";
 
 const Stocks = () => {
 
+    const inputStock = document.querySelector(".stockInput");
+    const submitStockChange = document.querySelector(".submitStockChange");
+
     const[chartValuesX, setChartValuesX] = useState([]);
     const[chartValuesY, setChartValuesY] = useState([]);
+    const[currentStock, setCurrentStock] = useState("");
 
     const fetchStockMarket = () => {
         const API_KEY = `P6C0Q88ZI50BWU1H`;
-        let stockSymbol = `IBM`;
-        let fetch_API = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${stockSymbol}&outputsize=compact&apikey=${API_KEY}`;
+        let stockSymbol = `NKE`;
+        let fetch_API = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${currentStock}&outputsize=compact&apikey=${API_KEY}`;
 
         fetch(fetch_API).then(response => {
             return response.json();
@@ -22,6 +27,13 @@ const Stocks = () => {
                 setChartValuesY(prevState => [...prevState, data[`Time Series (Daily)`][key][`4. close`]])
             }
         })
+    };
+
+    const changeStock = (e) => {
+        e.preventDefault();
+        setChartValuesX(prevState => []);
+        setChartValuesY(prevState => []);
+        fetchStockMarket();
     };
 
     useEffect(() => {
@@ -45,6 +57,10 @@ const Stocks = () => {
                     layout={{width: 740, height: 480, title: 'A Fancy Plot'}}
                 />
             </p>
+            <form>
+                <input className={"stockInput"} type={"text"} value={currentStock} onChange={event => setCurrentStock(event.target.value)} />
+                <button onClick={changeStock} type={"submit"} className={"submitStockChange"}>Submit</button>
+            </form>
 
         </>
     )
